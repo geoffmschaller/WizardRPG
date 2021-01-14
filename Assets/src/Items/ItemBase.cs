656e35424622c92d50c5504de81b40f209f93e3d
Geoff <geoffmschaller@gmail.com>
2021-01-14 13:09:06 -0800

    First Commit. Player Movement with Nav Mesh.

---
+++ Assets/src/Items/ItemBase.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class ItemBase : MonoBehaviour
+{
+
+	public void InteractWithItem()
+	{
+		Debug.Log("Interacting With Item");
+	}
+	
+}
---
+++ Assets/src/Player/PlayerDestination.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDestination
+{
+
+	private ItemBase _itemAtDestination;
+
+	public void SetDestination()
+	{
+		_itemAtDestination = null;
+	}
+	
+	public void SetDestination(ItemBase item)
+	{
+		_itemAtDestination = item;
+	}
+
+	public void ArriveAtDestination()
+	{
+		if (_itemAtDestination != null)
+		{
+			_itemAtDestination.InteractWithItem();
+		}
+		else
+		{
+			Debug.Log("Arrived at location!");
+		}
+	}
+
+}
